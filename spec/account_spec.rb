394f1bd72b62c802ce2df17453036ace228ffb63
Tim <TimCPB@github.com>
2020-07-14 00:41:29 +0100

    passing Account unit test 'initializes with a balance of 0'

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,9 @@ describe Account do
     account = Account.new
     expect(account).to be
   end
+
+  it "initializes with a balance of 0" do
+    account = Account.new
+    expect(account.balance). to eq(0)
+  end
 end
