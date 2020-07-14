7833d7632eba9163cae35457d484c078a3f2a9be
Tim <TimCPB@github.com>
2020-07-14 22:46:59 +0100

    refactod of generate_statement method, extracting 2 responsabilities into private methods

--- lib/statement.rb
+++ lib/statement.rb
@@ -11,12 +11,23 @@ class Statement # :nodoc:
 
   def generate_statement
     array = @transaction_array.map do |transaction|
-      if transaction.type == 'deposit'
+      if deposit?(transaction.type)
         "#{transaction.date} || #{transaction.amount} || || #{transaction.new_balance}"
       else
         "#{transaction.date} || || #{transaction.amount} || #{transaction.new_balance}"
       end
     end
+    join_with_header(array)
+  end
+
+  private
+
+  def deposit?(type)
+    type == 'deposit'
+  end
+
+  def join_with_header(array)
     "date || credit || debit || balance\n" + array.join("\n")
   end
+
 end
