eead5a5650e298edc47166e9ed006a166f715065
Tim <TimCPB@github.com>
2020-07-14 11:23:21 +0100

    linting with rubocop

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-class Transaction
-
+class Transaction # :nodoc:
   attr_reader :amount, :new_balance
 
   def initialize(amount, new_balance)
     @amount = amount
     @new_balance = new_balance
   end
-
 end
