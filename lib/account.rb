c537ccdd6c6ff55228a2b4504d61f153e471e37e
Tim <TimCPB@github.com>
2020-07-14 10:03:24 +0100

    passing 'receives a deposit' unit test in account_spec.rb

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,14 @@
-class Account
+# frozen_string_literal: true
 
+# this is now the top level class
+class Account
   attr_reader :balance
 
   def initialize
     @balance = 0
+  end
+
+  def deposit(amount)
   end
   
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,16 +1,21 @@
+# frozen_string_literal: true
+
 require 'account'
 
 describe Account do
-
-  context "A new account is set up" do
+  context 'a new account is set up' do
     account = Account.new
 
-    it "exists (can be initialized)" do
+    it 'exists (can be initialized)' do
       expect(account).to be
     end
 
-    it "initializes with a balance of 0" do
+    it 'initializes with a balance of 0' do
       expect(account.balance). to eq(0)
+    end
+
+    it 'receives a deposit' do
+      expect(account).to respond_to(:deposit).with(1).argument
     end
   end
 end
