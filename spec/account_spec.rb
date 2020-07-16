240776c73160ff2bf8a0d8ce0d2174def60de157
Tim <TimCPB@github.com>
2020-07-16 12:08:39 +0100

    refactor of account_spec.rb to isolate tests and remove redundant tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,58 +5,48 @@ require 'account'
 describe Account do
   let(:account) { Account.new }
 
-  it 'exists (can be initialized)' do
-    expect(account).to be
-  end
-
-  it 'initializes with a balance of 0' do
-    expect(account.balance). to eq(0)
-  end
-
-  it 'receives a deposit' do
-    expect(account).to respond_to(:deposit).with(1).argument
-  end
-
-  it 'records the transaction history' do
-    expect(account.transaction_history).to eq([])
-  end
-
-  it 'shows a total balance after multiple deposits' do
-    account.deposit(20)
-    account.deposit(30)
-    expect(account.balance).to eq(50)
-  end
-
-  it 'shows a total balance after multiple deposits and withdrawls' do
-    account.deposit(40)
-    account.withdraw(30)
-    expect(account.balance).to eq(10)
-  end
-
   context 'after creating several transactions' do
-    account2 = Account.new
-    account2.deposit(50)
-    account2.deposit(30)
-    account2.withdraw(40)
+    it 'shows a total balance after multiple deposits' do
+      account.deposit(20)
+      account.deposit(30)
+      expect(account.balance).to eq(50)
+    end
+
+    it 'shows a total balance after multiple deposits and withdrawls' do
+      account.deposit(40)
+      account.withdraw(30)
+      expect(account.balance).to eq(10)
+    end
 
     it 'stores all transactions in the transaction_history' do
-      expect(account2.transaction_history.length).to eq(3)
+      account.deposit(50)
+      account.withdraw(20)
+      account.deposit(30)
+      expect(account.transaction_history.length).to eq(3)
     end
 
     it 'depositing an amount creates a new instance of Transaction class' do
-      expect(account2.transaction_history[1]).to be_instance_of(Transaction)
+      account.deposit(50)
+      account.withdraw(40)
+      expect(account.transaction_history[0]).to be_instance_of(Transaction)
     end
 
     it 'withdrawing an amount creates a new instance of Transaction class' do
-      expect(account2.transaction_history[2]).to be_instance_of(Transaction)
+      account.deposit(60)
+      account.withdraw(40)
+      expect(account.transaction_history[1]).to be_instance_of(Transaction)
     end
 
     it "making a withdrawal creates an instance of Transaction with the type attribute of 'withdrtawl'" do
-      expect(account2.transaction_history[2].type).to eq('withdrawal')
+      account.deposit(80)
+      account.withdraw(30)
+      expect(account.transaction_history[1].type).to eq('withdrawal')
     end
 
     it 'print_statement outputs a statement' do
-      expect { account2.print_statement }.to output.to_stdout
+      account.withdraw(200)
+      account.deposit(50)
+      expect { account.print_statement }.to output.to_stdout
     end
   end
 end
