17877ea249beafb89c0b44db5fa85414f5e72dac
Tim <TimCPB@github.com>
2020-07-16 15:28:03 +0100

    modification to transaction.rb, extracting some formatting to a private methods

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,9 +4,19 @@ class Transaction # :nodoc:
   attr_reader :date, :amount, :new_balance, :type
 
   def initialize(amount, new_balance, type = 'deposit')
-    @date = Time.now.strftime('%d/%m/%Y')
-    @amount = '%.2f' % amount
-    @new_balance = '%.2f' % new_balance
+    @date = format_time(Time.now)
+    @amount = format(amount)
+    @new_balance = format(new_balance)
     @type = type
+  end
+
+  private
+
+  def format(number)
+    '%.2f' % number
+  end
+
+  def format_time(time)
+    time.strftime('%d/%m/%Y')
   end
 end
